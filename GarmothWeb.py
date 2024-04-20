bf505f3fcbef8ac4723cfb123fbb4f91ffe54353
rcdong <richard_dong@sfu.ca>
2024-04-19 18:24:14 -0700

    obtained codes from garmoth.com

---
+++ GarmothWeb.py
@@ -0,0 +1,41 @@
+
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
+class GarmothWeb():
+
+    baseURL = "https://garmoth.com"
+    coupons = []
+
+    def __init__(self):
+
+        # get codes
+        options = Options()
+        options.add_argument('-headless')
+        browser = Firefox(options=options)
+
+        browser.get(self.baseURL)
+
+        seeMoreButton = browser.find_element(By.XPATH, "/html/body/div[1]/div/main/div[2]/div[1]/div[1]/section[2]/div[1]/div/a")
+        seeMoreButton.click()
+
+        availableCouponsContainer = browser.find_element(By.XPATH, "/html/body/div[1]/div/main/div[2]/div[2]/section[1]/div")
+        couponContainers = availableCouponsContainer.find_elements(By.CSS_SELECTOR, "input[id]")
+        for coupon in couponContainers:
+            self.coupons.append(coupon.get_attribute("id"))
+
+        browser.close()
+
+    def getCouponCodes(self):
+        return self.coupons
+
+
+if __name__ == "__main__":
+    
+    garmothWeb = GarmothWeb()
+    print(garmothWeb.getCouponCodes())
+    pass
+
+    
+    
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # bdo-code-redeem
-(In progress) Automatically redeems the gift codes for the game bdo
+(In progress) Automatically redeems the gift codes for the game bdo (Never missing those high cron codes again)
+
+
+
+Design:
+    1. Idea is to utilize garmoth.com as the website where we will obtain the codes. Credits to them. The information is stored as HTML so we will use beautiful soup to obtain the information.
+        - note: As the plan is to run this as a cron job, we want to check
+        for codes that have already been redeemed. Perhaps we will keep a 
+        local file to track this informatino
+
+    2. The code redemption will be on BDO's official website. One will require user account information (My usage is on Steam) to access the code redemption option. This step will require more interaction, so I believe selenium will be required to achieve this,
+        - note: Steam Guard may be trouble some to handle. Browers uses a cookie to make it so we don't have to do passcode repeatedly. Look into how this can be accomplished for this script
+
+
+Apr19: Obtained codes from garmoth.com
